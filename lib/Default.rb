2aa90c5bde36f6b03f01f3901cb70d0c72d9cb66
Rob Frawley 2nd <rmf@src.run>
2016-06-29 16:55:28 -0400

    renamed Defaults to Default

---
+++ lib/Default.rb
@@ -0,0 +1,20 @@
+
+#
+# This file is part of the `src-run/latex-style-builder` project.
+#
+# (c) Rob Frawley 2nd <rmf@src.run>
+#
+# For the full copyright and license information, please view the LICENSE.md
+# file that was distributed with this source code.
+#
+
+class Default
+
+  RESOURCE  = 'http://fortawesome.github.io/Font-Awesome/cheatsheet/'
+  PATH      = 'FontAwesome.sty'
+  C_PROJECT = 'src-run/latex-style-builder'
+  C_AUTHOR  = 'Rob Frawley 2nd <rmf@src.run>'
+
+end
+
+# EOF
